e67a33ed22ed403c4cac916786ca418fccbcce71
Ruman Hossain <ruman.cse.brur@gmail.com>
2023-06-17 04:26:52 +0600

    Use Styles Not Working

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,24 @@
 import { Container, Typography } from '@mui/material'
+import { createStyles, makeStyles } from '@mui/styles';
 import React from 'react'
 import {GridContainer} from '../components/GridContainer'
+import { TyphographyComponent } from './TyphographyComponent';
 
+const useStyles = makeStyles(()=>{
+    createStyles({
+        container:{
+            backgroundColor: '#ff8000',
+            height:'20px'
+        }
+    });
+});
 export const MainContainer = () => {
+    const classes = useStyles();
   return (
     <main>
-        <div>
+        <div className={classes.container}>
             <Container maxWidth='sm' style={{marginTop:'100px' }}>
-                <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
-                    Photo Album
-                </Typography>
+                <TyphographyComponent/>
                 <Typography variant='h5'  align='center' color='textSecondary'  paragraph>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tempora expedita beatae autem sunt doloremque iure nostrum consequuntur suscipit facere repudiandae, laborum provident quaerat inventore sequi? Asperiores maxime sapiente fuga?
                 </Typography>
