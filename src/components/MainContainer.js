5270952b5876dd04b6aaed417d9ec735fb359e6f
Ruman Hossain <ruman.cse.brur@gmail.com>
2023-06-17 04:35:43 +0600

    Typography GridContainer container Button

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,22 +1,12 @@
 import { Container, Typography } from '@mui/material'
-import { createStyles, makeStyles } from '@mui/styles';
 import React from 'react'
 import {GridContainer} from '../components/GridContainer'
 import { TyphographyComponent } from './TyphographyComponent';
 
-const useStyles = makeStyles(()=>{
-    createStyles({
-        container:{
-            backgroundColor: '#ff8000',
-            height:'20px'
-        }
-    });
-});
 export const MainContainer = () => {
-    const classes = useStyles();
   return (
     <main>
-        <div className={classes.container}>
+        <div>
             <Container maxWidth='sm' style={{marginTop:'100px' }}>
                 <TyphographyComponent/>
                 <Typography variant='h5'  align='center' color='textSecondary'  paragraph>
