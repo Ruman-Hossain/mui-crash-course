f9e5ab37df09a6a8a04b83cfe4194456cf591ada
Ruman Hossain <ruman.cse.brur@gmail.com>
2023-06-17 04:37:30 +0600

    Create Styles Not working

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,23 @@
+
 import { Container, Typography } from '@mui/material'
+import { createStyles, makeStyles } from '@mui/styles';
 import React from 'react'
 import {GridContainer} from '../components/GridContainer'
 import { TyphographyComponent } from './TyphographyComponent';
 
+const useStyles = makeStyles(()=>{
+    createStyles({
+        container:{
+            backgroundColor: '#ff8000',
+            height:'20px'
+        }
+    });
+});
 export const MainContainer = () => {
+    const classes = useStyles();
   return (
     <main>
-        <div>
+        <div className={classes.container}>
             <Container maxWidth='sm' style={{marginTop:'100px' }}>
                 <TyphographyComponent/>
                 <Typography variant='h5'  align='center' color='textSecondary'  paragraph>
