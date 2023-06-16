c3705625bb1c66c52bf17d6305be4eaed0fc1906
Ruman Hossain <ruman.cse.brur@gmail.com>
2023-06-17 04:25:30 +0600

    Typography Component changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { TyphographyComponent } from './components/TyphographyComponent';
 import { AppBarComponent } from './components/AppBarComponent';
 import { MainContainer } from './components/MainContainer';
 
 function App() {
   return (
     <div className="App">
-      <TyphographyComponent/>
       <AppBarComponent/>
       <MainContainer/>
     </div>
--- src/components/TyphographyComponent.js
+++ src/components/TyphographyComponent.js
@@ -3,12 +3,13 @@ import { Typography } from '@mui/material'
 export const TyphographyComponent = () => {
   return (
     <>
-      <Typography variant="h1">Heading . h1</Typography>
+      <Typography variant='h2'>Photo Album</Typography>
+      {/* <Typography variant="h1">Heading . h1</Typography>
       <Typography variant="h2">Heading . h2</Typography>
       <Typography variant="h3">Heading . h3</Typography>
       <Typography variant="h4">Heading . h4</Typography>
       <Typography variant="h5">Heading . h5</Typography>
-      <Typography variant="h6">Heading . h6</Typography>
+      <Typography variant="h6">Heading . h6</Typography> */}
     </>
   )
 }
